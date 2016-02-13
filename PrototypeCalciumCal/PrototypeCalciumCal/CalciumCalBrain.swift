195823a256ae6d37294152e280c462f0ed0b6ab7
Chongwei <lewgreat@gmail.com>
2016-02-13 23:30:22 +0800

    Tweaking MVC.

---
+++ PrototypeCalciumCal/PrototypeCalciumCal/CalciumCalBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalciumCalBrain.swift
+//  PrototypeCalciumCal
+//
+//  Created by Liu's MBA on 2/13/16.
+//  Copyright © 2016 dancite. All rights reserved.
+//
+
+import Foundation
+
+class calciumBrain{
+    
+}
